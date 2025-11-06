3a0eb372895eefa41241d8d139696c2f3322294e
domonk-glitch <domonk@gmail.com>
2025-11-06 09:10:52 -0500

    Added logging from overnight update.

--- main.py
+++ main.py
@@ -3,16 +3,30 @@
 # throughout this file
 import pygame
 from constants import *
+from logger import log_state
 
 def main():
     pygame.init
+#    print('Starting Asteroids!')
+#    print('Screen width: 1280')
+#    print('Screen height: 720') 
+#    log_state()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+#    clock = pygame.time.Clock()
+#    dt = 0
+
     while True:
+        log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+#
+        screen.fill("black")
         pygame.display.flip()
+
+#       dt = clock.tick(60) / 1000
+
 
 
 
