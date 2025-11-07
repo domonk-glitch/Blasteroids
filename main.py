42ba8a42a6717e154b62073dc0693c0719596555
domonk-glitch <domonk@gmail.com>
2025-11-07 10:58:07 -0500

    Player avatar and rotation.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init
@@ -12,8 +13,9 @@ def main():
 #    print('Screen height: 720') 
 #    log_state()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-#    clock = pygame.time.Clock()
-#    dt = 0
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         log_state()
@@ -21,11 +23,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-#
+        
+        player.update(dt)
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-#       dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
 
 
